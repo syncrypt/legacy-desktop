e799682b48d81827c5f2bc2e19ad6a004d414111
Christopher Bertels <bakkdoor@flasht.de>
2016-08-27 18:48:47 +0200

    Add semicolons

--- app/index.js
+++ app/index.js
@@ -9,13 +9,13 @@ import configureStore from './store/configureStore';
 import './app.global.css';
 import { remote } from 'electron';
 import path from 'path';
-import child_process from 'child_process'
+import child_process from 'child_process';
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
 
 const appPath = path.dirname(remote.app.getAppPath());
-const daemonPath = path.join(appPath, "app", "client", "syncrypt_daemon")
+const daemonPath = path.join(appPath, "app", "client", "syncrypt_daemon");
 const daemonProcess = child_process.spawn(daemonPath);
 
 render(
