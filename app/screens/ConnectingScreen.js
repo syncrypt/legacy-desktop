1bd90a0a87c8b717ee9dc80acb59461c5d6cc637
Christopher Bertels <bakkdoor@flasht.de>
2016-09-11 21:35:37 +0200

    Add gradient animation to ConnectingScreen

--- app/screens/ConnectingScreen.js
+++ app/screens/ConnectingScreen.js
@@ -2,11 +2,29 @@ import React from 'react';
 import SyncryptComponent from '../components/SyncryptComponent';
 import { Link } from 'react-router';
 import './ConnectingScreen.css';
+import Granim from 'granim'
 
 class ConnectingScreen extends SyncryptComponent {
+  componentDidMount() {
+    var granimInstance = new Granim({
+      element: '#granim-canvas',
+      name: 'granim',
+      opacity: [1, 1],
+      states : {
+        "default-state": {
+          gradients: [
+            ['#3B6491', '#F3967F'],
+            ['#f3727f', '#F8B194']
+          ]
+        }
+      }
+    });
+
+  }
   render() {
     return (
       <div className="connecting-screen">
+        <canvas id="granim-canvas"></canvas>
         <div className="banner">
         </div>
       </div>
