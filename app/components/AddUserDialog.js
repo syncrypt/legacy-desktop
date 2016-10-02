fde50e27893af0916628c783da72c6567c9ecfb2
Hannes Gräuler <graeuler@geoplex.de>
2016-10-02 13:56:18 +0200

    Style AddUserDialog

--- app/components/AddUserDialog.js
+++ app/components/AddUserDialog.js
@@ -2,36 +2,45 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Modal, Button, Checkbox, FormGroup } from 'react-bootstrap'
 import UserIcon from './UserIcon';
+import './AddUserDialog.css';
 
 class AddUserDialog extends React.Component {
-    render () {
-        console.log(this.props.userkeys);
-        return <Modal show={this.props.show}>
-            <Modal.Header>
-                User hinzufügen
-            </Modal.Header>
-            <Modal.Body>
-                <UserIcon email={this.props.email} />
-                {this.props.email}
-                <div>
-                    <h4>Schlüssel</h4>
-                    {this.props.userkeys.data.map((userkey) =>
-                        <div>
-                            <FormGroup>
-                                <Checkbox inline checked>{userkey.description} ({userkey.fingerprint})</Checkbox>
-                            </FormGroup>
-                        </div>
-                    )}
-                </div>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button className="btn">Abbrechen</Button>
-                <Button className="btn btn-primary">User hinzufügen</Button>
-            </Modal.Footer>
-        </Modal>
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fingerprints: []
     }
+  }
+
+  render () {
+    return <Modal show={this.props.show} className='add-user-dialog'>
+      <Modal.Header>
+        Invite User
+      </Modal.Header>
+      <Modal.Body>
+        <UserIcon email={this.props.email} />
+        {this.props.email}
+        <div>
+          <h4>Device keys</h4>
+          {this.props.userkeys.loading ? "Loading..." :
+            this.props.userkeys.data.map((userkey) =>
+              <div>
+                <FormGroup>
+                  <Checkbox inline checked>{userkey.description} ({userkey.fingerprint})</Checkbox>
+                </FormGroup>
+              </div>
+            )}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="btn">Cancel</Button>
+          <Button className="btn btn-primary">Invite user and send keys</Button>
+        </Modal.Footer>
+      </Modal>
+  }
 }
 
 export default connect(({ userkeys }) => ({
-    userkeys: userkeys
+  userkeys: userkeys
 }))(AddUserDialog);
